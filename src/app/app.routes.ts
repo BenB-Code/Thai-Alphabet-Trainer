43be7bdfe8592a35d8894b19d5c74a7b44b1ebef
Benjamin <benjamin.bats.dev@gmail.com>
2026-01-17 00:30:48 +0100

    add basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: App,
+  },
+  {
+    path: 'quiz',
+    loadComponent: () => import('./features/quiz/quiz').then(m => m.Quiz),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
