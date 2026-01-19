035dde374474115999f9c7b51eefba1df4f48e10
Benjamin <benjamin.bats.dev@gmail.com>
2026-01-19 18:53:27 +0100

    first shot of card component. Still shit to doo.

--- src/app/features/card/card.ts
+++ src/app/features/card/card.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core';
+import { MatCard, MatCardContent } from '@angular/material/card';
+import { ThaiConsonant, ThaiVowel } from '../../shared/models';
+import { StateService } from '../../services/state-service/state-service';
+import { COLORS, PRIMARY } from '../../shared/constants';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [MatCard, MatCardContent],
   templateUrl: './card.html',
   styleUrl: './card.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[style.--card-color]': 'color()',
+  },
 })
-export class Card {}
+export class Card {
+  protected readonly stateService: StateService = inject<StateService>(StateService);
+
+  letter = input.required<ThaiConsonant | ThaiVowel>();
+  color = input<COLORS>(PRIMARY);
+  isActive = computed(() => this.stateService.total().has(this.letter()));
+
+  toggleLetter(): void {
+    this.stateService.toggleLetter(this.letter());
+  }
+}
