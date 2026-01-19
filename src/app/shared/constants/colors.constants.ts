607ba01529b7ffb50679491b9d95b21594bb0d71
Benjamin <benjamin.bats.dev@gmail.com>
2026-01-19 18:12:28 +0100

    create custom colors, exporting way

---
+++ src/app/shared/constants/colors.constants.ts
@@ -0,0 +1,20 @@
+export const GREEN = 'var(--color-green)';
+export const GREEN_BLUE = 'var(--color-green-blue)';
+export const ORANGE = 'var(--color-orange)';
+export const PURPLE = 'var(--color-purple)';
+export const RED = 'var(--mat-sys-error)';
+export const PRIMARY = 'var(--mat-sys-primary)';
+export const SECONDARY = 'var(--mat-sys-secondary)';
+export const TERTIARY = 'var(--mat-sys-tertiary)';
+export const NEUTRAL = 'var(--mat-sys-neutral)';
+
+export type COLORS =
+  | typeof GREEN
+  | typeof GREEN_BLUE
+  | typeof ORANGE
+  | typeof PURPLE
+  | typeof RED
+  | typeof PRIMARY
+  | typeof SECONDARY
+  | typeof TERTIARY
+  | typeof NEUTRAL;
