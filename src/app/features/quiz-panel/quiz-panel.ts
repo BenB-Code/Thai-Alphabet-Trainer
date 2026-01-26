659532ea574bcd300aec1513bd2b2aa85110b1a1
Benjamin <benjamin.bats.dev@gmail.com>
2026-01-26 13:00:27 +0100

    quiz-panel factorisation

--- src/app/features/quiz-panel/quiz-panel.ts
+++ src/app/features/quiz-panel/quiz-panel.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatCard } from '@angular/material/card';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+import { QuizPanelRecap } from '../quiz-panel-recap/quiz-panel-recap';
+import { QuizPanelForm } from '../quiz-panel-form/quiz-panel-form';
 
 @Component({
   selector: 'app-quiz-panel',
-  imports: [],
+  imports: [MatCard, TranslatePipe, MatRadioModule, MatButtonModule, QuizPanelRecap, QuizPanelForm],
   templateUrl: './quiz-panel.html',
   styleUrl: './quiz-panel.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class QuizPanel {}
