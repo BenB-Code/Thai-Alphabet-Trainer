5fe6b93ad3fff5abb3e026c33ae72f99a6408d56
Benjamin <benjamin.bats.dev@gmail.com>
2026-01-26 16:59:21 +0100

    done language switcher with tests

--- src/app/services/i18n-service/i18n-service.spec.ts
+++ src/app/services/i18n-service/i18n-service.spec.ts
@@ -2,18 +2,38 @@ import { TestBed } from '@angular/core/testing';
 
 import { I18nService } from './i18n-service';
 import { provideZonelessChangeDetection } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { EN, FR } from '../../shared/constants';
 
 describe('I18nService', () => {
   let service: I18nService;
+  let translateServiceSpy: jasmine.SpyObj<TranslateService>;
 
   beforeEach(() => {
+    translateServiceSpy = jasmine.createSpyObj('TranslateService', ['use']);
+
     TestBed.configureTestingModule({
-      providers: [provideZonelessChangeDetection()],
+      imports: [TranslateModule.forRoot()],
+      providers: [
+        provideZonelessChangeDetection(),
+        {
+          provide: TranslateService,
+          useValue: translateServiceSpy,
+        },
+      ],
     });
     service = TestBed.inject(I18nService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should change lang from EN to FR', () => {
+    expect(service.activeLanguage()).toEqual(EN);
+
+    service.switchLanguage(FR);
+    expect(service.activeLanguage()).toEqual(FR);
+    expect(translateServiceSpy.use).toHaveBeenCalledWith(FR);
   });
 });
