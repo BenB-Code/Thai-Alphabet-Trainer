342eaccc248460974f409569ee3e6b1487891c29
Benjamin <benjamin.bats.dev@gmail.com>
2026-01-26 16:28:17 +0100

    small style and labeling

--- src/app/services/quiz-service/quiz-service.spec.ts
+++ src/app/services/quiz-service/quiz-service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { QuizService } from './quiz-service';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('QuizService', () => {
   let service: QuizService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideZonelessChangeDetection()],
+    });
     service = TestBed.inject(QuizService);
   });
 
