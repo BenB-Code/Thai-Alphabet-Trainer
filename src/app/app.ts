44ac980552ff270963a686d83a8d21c46f807a4e
Benjamin <benjamin.bats.dev@gmail.com>
2026-01-26 14:10:48 +0100

    style cleaning and add quiz panel

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { LettersTab } from './features/letters-tab/letters-tab';
+import { QuizPanel } from './features/quiz-panel/quiz-panel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [MatButtonModule, LettersTab],
+  imports: [MatButtonModule, LettersTab, QuizPanel],
 })
 export class App {}
