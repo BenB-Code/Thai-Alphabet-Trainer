b1c5d95e12374678a53e8d696ed36da150ed4df2
Benjamin <benjamin.bats.dev@gmail.com>
2026-01-27 11:29:54 +0100

    simple nav service

---
+++ src/app/services/navigation-service/navigation-service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+
+import { NavigationService } from './navigation-service';
+import { provideZonelessChangeDetection } from '@angular/core';
+
+describe('NavigationService', () => {
+  let service: NavigationService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideZonelessChangeDetection()],
+    });
+    service = TestBed.inject(NavigationService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
