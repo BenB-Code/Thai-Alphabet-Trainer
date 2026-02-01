36530b5f3f6f3b9ee19789d06e397d25674e703f
Benjamin <benjamin.bats.dev@gmail.com>
2026-02-01 19:23:43 +0100

    quiz service to handle all nav behavior and quiz generation

--- src/app/services/quiz-service/quiz-service.spec.ts
+++ src/app/services/quiz-service/quiz-service.spec.ts
@@ -2,6 +2,8 @@ import { TestBed } from '@angular/core/testing';
 
 import { QuizService } from './quiz-service';
 import { provideZonelessChangeDetection } from '@angular/core';
+import { FINISHED, IN_PROGRESS, LATIN, MIXED, PAUSE, QUIZ_FORM_BASE_CONF, THAI } from '../../shared/constants';
+import { THAI_CONSONANTS } from '../../data';
 
 describe('QuizService', () => {
   let service: QuizService;
@@ -15,5 +17,128 @@ describe('QuizService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('generateQuizList', () => {
+    it('should generate an random list based on quantity', () => {
+      service.quizSettings.update(() => ({
+        display: LATIN,
+        questions: 22,
+        selected: THAI_CONSONANTS,
+        delay: QUIZ_FORM_BASE_CONF.delay[2],
+        randomized: [],
+      }));
+
+      expect(service.quizSettings().randomized.length).toEqual(0);
+      service.generateQuizList();
+
+      expect(service.quizSettings().randomized.length).toEqual(22);
+      expect(service.quizSettings().randomized[0].display).toEqual(LATIN);
+    });
+  });
+
+  describe('generateDisplayType', () => {
+    it('should return the same display setting', () => {
+      service.generateDisplayType();
+
+      expect(service.generateDisplayType()).toBe(service.quizSettings().display);
+    });
+
+    it('should return the randomly selected display setting', () => {
+      service.quizSettings.update(set => ({
+        ...set,
+        display: MIXED,
+      }));
+      spyOn(Math, 'random').and.returnValues(0.4, 0.7);
+
+      expect(service.generateDisplayType()).toContain(LATIN);
+      expect(service.generateDisplayType()).toContain(THAI);
+    });
+  });
+
+  describe('incrProgress', () => {
+    beforeEach(() => {
+      service.generateQuizList();
+    });
+
+    it('should update canGoForward, canGoBack and index', () => {
+      service.index.update(() => 5);
+
+      service.incrProgress();
+
+      expect(service.canGoBack()).toEqual(true);
+      expect(service.canGoForward()).toEqual(true);
+      expect(service.index()).toEqual(6);
+    });
+
+    it('should update state', () => {
+      service.index.update(() => service.quizSettings().questions - 2);
+
+      service.incrProgress();
+
+      expect(service.state()).toEqual(FINISHED);
+      expect(service.canGoBack()).toEqual(true);
+      expect(service.canGoForward()).toEqual(true);
+      expect(service.index()).toEqual(9);
+    });
+
+    it('should update canGoForward', () => {
+      service.index.update(() => 999);
+
+      service.incrProgress();
+
+      expect(service.canGoForward()).toEqual(false);
+    });
+  });
+
+  describe('decrProgress', () => {
+    it('should update index and canGoBack', () => {
+      service.index.update(() => 5);
+      service.decrProgress();
+
+      expect(service.canGoBack()).toEqual(true);
+      expect(service.index()).toEqual(4);
+    });
+
+    it('should update canGoBack', () => {
+      service.index.update(() => 1);
+      service.decrProgress();
+
+      expect(service.canGoBack()).toEqual(false);
+      expect(service.index()).toEqual(0);
+    });
+  });
+
+  describe('toggleProgressState', () => {
+    it('should change state to PAUSE', () => {
+      service.toggleProgressState();
+
+      expect(service.state()).toEqual(PAUSE);
+    });
+
+    it('should change state to IN_PROGRESS', () => {
+      service.toggleProgressState();
+      service.toggleProgressState();
+
+      expect(service.state()).toEqual(IN_PROGRESS);
+    });
+  });
+
+  describe('resetQuiz', () => {
+    it('should set the default value', () => {
+      service.isValid.update(() => true);
+      service.canGoBack.update(() => true);
+      service.canGoForward.update(() => false);
+      service.index.update(() => 12);
+      service.state.update(() => PAUSE);
+
+      service.resetQuiz();
+
+      expect(service.isValid()).toEqual(false);
+      expect(service.canGoBack()).toEqual(false);
+      expect(service.canGoForward()).toEqual(true);
+      expect(service.index()).toEqual(0);
+      expect(service.state()).toEqual(IN_PROGRESS);
+    });
   });
 });
--- src/app/services/quiz-service/quiz-service.ts
+++ src/app/services/quiz-service/quiz-service.ts
@@ -1,25 +1,83 @@
 import { Injectable, signal } from '@angular/core';
-import { QuizFormat } from '../../shared/models';
+import { ProgressState, QuizFormat, ThaiCharacter } from '../../shared/models';
+import { FINISHED, IN_PROGRESS, LATIN, MIXED, PAUSE, QUIZ_FORM_BASE_CONF, THAI } from '../../shared/constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class QuizService {
+  quizSettings = signal<QuizFormat>({
+    display: LATIN,
+    questions: 10,
+    selected: [],
+    delay: QUIZ_FORM_BASE_CONF.delay[2],
+    randomized: [],
+  });
   isValid = signal<boolean>(false);
-  quizSettings = signal<QuizFormat | null>(null);
+  canGoBack = signal<boolean>(false);
+  canGoForward = signal<boolean>(true);
+  index = signal<number>(0);
+  state = signal<ProgressState>(IN_PROGRESS);
 
-  generateList(): void {
-    console.log(this.quizSettings());
+  generateQuizList() {
+    let newList: ThaiCharacter[] = [];
+    const quantity = this.quizSettings().questions;
+    const selected = this.quizSettings().selected;
+
+    for (let i = 0; i < quantity; i++) {
+      const randomIndex = Math.floor(Math.random() * selected.length);
+      let letter = selected[randomIndex];
+
+      letter = {
+        ...letter,
+        display: this.generateDisplayType(),
+      };
+      newList = [...newList, letter];
+    }
+    this.quizSettings.update(setting => ({ ...setting, randomized: newList }));
   }
-  //
-  // randomiseList(list) {
-  //   const newList = new Set();
-  //   const quantity = this.quizSettings()?.questions || 1;
-  //   const selected = this.quizSettings()?.selected || new Set();
-  //
-  //   for (let i = 0; i < quantity; i++) {
-  //     const randomIndex = Math.floor(Math.random() * selected.size);
-  //     newList.add(selected.keys(randomIndex));
-  //   }
-  // }
+
+  generateDisplayType() {
+    if (this.quizSettings()?.display === MIXED) {
+      return Math.random() < 0.5 ? LATIN : THAI;
+    }
+    return this.quizSettings()?.display;
+  }
+
+  setCanGoBack() {
+    if (this.index() === 0) {
+      this.canGoBack.update(() => false);
+    } else {
+      this.canGoBack.update(() => true);
+    }
+  }
+
+  incrProgress() {
+    this.index.update(i => i + 1);
+    if (this.index() === this.quizSettings().questions - 1) {
+      this.state.update(() => FINISHED);
+    } else if (this.index() < this.quizSettings().questions - 1) {
+      this.canGoForward.update(() => true);
+    } else {
+      this.canGoForward.update(() => false);
+    }
+    this.setCanGoBack();
+  }
+
+  decrProgress() {
+    this.index.update(i => i - 1);
+    this.setCanGoBack();
+  }
+
+  toggleProgressState() {
+    this.state.update(state => (state === IN_PROGRESS ? PAUSE : IN_PROGRESS));
+  }
+
+  resetQuiz() {
+    this.isValid.update(() => false);
+    this.canGoBack.update(() => false);
+    this.canGoForward.update(() => true);
+    this.index.update(() => 0);
+    this.state.update(() => IN_PROGRESS);
+  }
 }
