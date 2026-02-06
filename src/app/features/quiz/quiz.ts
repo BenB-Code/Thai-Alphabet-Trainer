74a36d6f30c785128040c6b2a6857f5f8e243e34
Benjamin <benjamin.bats.dev@gmail.com>
2026-02-06 16:55:01 +0100

    add quiz-progress-bar

--- src/app/features/quiz/quiz.ts
+++ src/app/features/quiz/quiz.ts
@@ -3,10 +3,11 @@ import { QuizCard } from '../quiz-card/quiz-card';
 import { QuizHeader } from '../quiz-header/quiz-header';
 import { QuizNav } from '../quiz-nav/quiz-nav';
 import { QuizService } from '../../services/quiz-service/quiz-service';
+import { QuizProgressBar } from '../quiz-progress-bar/quiz-progress-bar';
 
 @Component({
   selector: 'app-quiz',
-  imports: [QuizCard, QuizHeader, QuizNav],
+  imports: [QuizCard, QuizHeader, QuizNav, QuizProgressBar],
   templateUrl: './quiz.html',
   styleUrl: './quiz.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
