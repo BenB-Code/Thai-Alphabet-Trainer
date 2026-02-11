db58e3ef0b000d461130c3631ba1c62de7c6d57b
Benjamin <benjamin.bats.dev@gmail.com>
2026-02-11 19:32:43 +0100

    quick contact icon

--- src/app/features/banner/banner.ts
+++ src/app/features/banner/banner.ts
@@ -1,11 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LangContainer } from '../lang-container/lang-container';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-banner',
-  imports: [LangContainer],
+  imports: [LangContainer, MatIconModule, MatButtonModule],
   templateUrl: './banner.html',
   styleUrl: './banner.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Banner {}
+export class Banner {
+  redirect(url: string): void {
+    window.open(url, '_self');
+  }
+}
