84d2af4decb3ac92416ef0cc5530380fce89771c
Benjamin <benjamin.bats.dev@gmail.com>
2026-02-13 19:02:57 +0100

    create switch selector component

---
+++ src/app/rework/common/switch-selector/switch-selector.ts
@@ -0,0 +1,49 @@
+import { ChangeDetectionStrategy, Component, input, output, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-selector',
+  imports: [],
+  templateUrl: './switch-selector.html',
+  styleUrl: './switch-selector.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SwitchSelector {
+  dark = input(false);
+  list = input<
+    {
+      label: {
+        display: boolean;
+        text: string;
+      };
+      icon: {
+        display: boolean;
+        path: string;
+        alt: string;
+        right: boolean;
+      };
+      id: number;
+    }[]
+  >([
+    {
+      label: {
+        display: true,
+        text: '',
+      },
+      icon: {
+        display: true,
+        path: '',
+        alt: '',
+        right: false,
+      },
+      id: 0,
+    },
+  ]);
+
+  activeItem = output<number>();
+  activeIndex = signal(0);
+
+  activate(id: number) {
+    this.activeIndex.set(id);
+    this.activeItem.emit(id);
+  }
+}
